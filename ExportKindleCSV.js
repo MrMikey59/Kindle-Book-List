104aaebfefa33a94c7f82dd32a7862c28eef6cb9
MrMikey59 <mikemyers59@hotmail.com>
2021-12-05 17:07:35 -0800

    Added Chrome problem note and possible correction to both ExportKindle files.

--- ExportKindleCSV.js
+++ ExportKindleCSV.js
@@ -44,4 +44,10 @@ getItemsList()
 items.forEach(item => {
   csvData += item.asin + '","' + item.title + '","' + item.authors + '\n'
 })
+// This fails in Chrome:
 window.location = 'data:text/csv;charset=utf8,' + encodeURIComponent(csvData)
+
+// Try this code  instead with Chrome:
+// booklist = 'data:text/csv;charset=utf8,' + encodeURIComponent(csvData);
+// var win = window.open();
+// win.document.write('<iframe src="' + booklist + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
--- ExportKindleTSV.js
+++ ExportKindleTSV.js
@@ -45,4 +45,10 @@ items.forEach(item => {
   tsvData += item.asin + '"\t"' + item.title + '"\t"' + item.authors + '"\t"' + item.productUrl + '"\t"' + item.percentageRead + '" \n"'
 })
 
+// This fails in Chrome
 window.location = 'data:text/csv;charset=utf8,' + encodeURIComponent(tsvData)
+
+// Try this code  instead with Chrome:
+// booklist = 'data:text/tsv;charset=utf8,' + encodeURIComponent(tsvData);
+// var win = window.open();
+// win.document.write('<iframe src="' + booklist + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
